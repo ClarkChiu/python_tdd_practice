ad41f4ced4a7c6df0d9293bc964c61be893773c9
Clark Chiu <freebooters@gmail.com>
2020-02-28 22:34:18 +0800

    step-6-refactoring

--- python_tdd_practice/calc.py
+++ python_tdd_practice/calc.py
@@ -8,7 +8,4 @@ class Calc:
         return a - b
 
     def mul(self, *args):
-        def mul2(a, b):
-            return a * b
-        
-        return reduce(mul2, args)
+        return reduce(lambda x, y: x * y, args)
