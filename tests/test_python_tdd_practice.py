c88b3ea618ec0eef856dd92c85d26fc1d16afd0d
Clark Chiu <freebooters@gmail.com>
2020-02-28 13:50:32 +0800

    Add step 2

--- tests/test_python_tdd_practice.py
+++ tests/test_python_tdd_practice.py
@@ -4,3 +4,8 @@ def test_add_two_numbers():
     c = Calc()
     res = c.add(4, 5)
     assert res == 9
+
+def test_add_three_numbers():
+    c = Calc()
+    res = c.add(4, 5, 10)
+    assert res == 19
