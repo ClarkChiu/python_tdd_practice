b2be0d68ddface25495f0edc6227ec4e14607218
Clark Chiu <freebooters@gmail.com>
2020-03-01 11:29:35 +0800

    step-9-2-upper-threshold

--- python_tdd_practice/calc.py
+++ python_tdd_practice/calc.py
@@ -18,5 +18,10 @@ class Calc:
         except ZeroDivisionError:
             return 'inf'
 
-    def avg(self, numbers):
-        return sum(numbers)/len(numbers)
+    def avg(self, numbers, ut=None):
+        if not ut:
+            ut = max(numbers)
+
+        new_list = [x for x in numbers if x <= ut]
+
+        return sum(new_list)/len(new_list)
