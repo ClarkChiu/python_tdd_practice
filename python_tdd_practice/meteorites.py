e808b9c311be085fcd3daf62eeeedc8fb7b47ae5
Clark Chiu <freebooters@gmail.com>
2020-03-03 22:08:12 +0800

    meteoritestats-class-added

---
+++ python_tdd_practice/meteorites.py
@@ -0,0 +1,17 @@
+import urllib.request
+import json
+
+from python_tdd_practice import calc
+
+URL = ("https://data.nasa.gov/resource/y77d-th95.json")
+
+
+class MeteoriteStats:
+    def get_data(self):
+        with urllib.request.urlopen(URL) as url:
+            return json.loads(url.read().decode())
+
+    def average_mass(self, data):
+        c = calc.Calc()
+        masses = [float(d['mass']) for d in data if 'mass' in d]
+        return c.avg(masses)
