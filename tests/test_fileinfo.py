e76bbd73acdd454d0150bf7e69cd4318ec17702f
Clark Chiu <freebooters@gmail.com>
2020-03-08 22:34:45 +0800

    patch-with-context-manager

--- tests/test_fileinfo.py
+++ tests/test_fileinfo.py
@@ -1,4 +1,5 @@
 from python_tdd_practice.fileinfo import FileInfo
+from unittest.mock import patch
 
 
 def test_init():
@@ -6,9 +7,17 @@ def test_init():
     fi = FileInfo(filename)
     assert fi.filename == filename
 
-
 def test_init_relative():
     filename = 'somefile.ext'
     relative_path = '../{}'.format(filename)
     fi = FileInfo(relative_path)
     assert fi.filename == filename
+
+def test_get_info():
+    filename = 'somefile.ext'
+    original_path = '../{}'.format(filename)
+    with patch('os.path.abspath') as abspath_mock:
+        test_abspath = 'some/abs/path'
+        abspath_mock.return_value = test_abspath
+        fi = FileInfo(original_path)
+        assert fi.get_info() == (filename, original_path, test_abspath)
